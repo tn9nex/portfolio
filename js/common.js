b636e32f323da612add41f3b15d1194f09bb9ca2
Anthony Sanchez <ddrmanthony@hotmail.com>
2016-09-20 12:26:14 -0500

    upadte portfolio

--- js/common.js
+++ js/common.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 
+   // Navigation buttons
+   $("#nav-home").click(function() {
+      $('html,body').animate({
+         scrollTop: $("#heading").offset().top
+      }, 1200);
+   });
+
+   $("#nav-about").click(function() {
+      $('html,body').animate({
+         scrollTop: $("#about").offset().top
+      }, 1200);
+   });
+
+   $("#nav-portfolio").click(function() {
+      $('html,body').animate({
+         scrollTop: $("#portfolio").offset().top
+      }, 1200);
+   });
+
+   $("#nav-contact").click(function() {
+      $('html,body').animate({
+         scrollTop: $("#contact").offset().top
+      }, 1200);
+   });
+
+   // Page buttons
    $("#about-me").click(function() {
       $('html,body').animate({
          scrollTop: $("#about").offset().top
@@ -11,6 +37,16 @@ $(document).ready(function() {
          scrollTop: $("#portfolio").offset().top
       }, 1200);
    });
+
+   $("#contact").click(function() {
+      $('html,body').animate({
+         scrollTop: $("#contact-form").offset().top
+      }, 1200);
+   });
+
+   //console.log($('#portfolio').innerHeight());
+   $('.about-gallery-item').height($('.about-gallery-content-container').innerHeight());
+
 
    $('#gallery-bar-container').customGalleryBar();
 });
